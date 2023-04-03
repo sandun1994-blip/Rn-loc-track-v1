b2db304efa75cb545e993eef680bacab14c0ee72
Sandun Tharuka <sandun.suntws@gmail.com>
2023-04-03 21:20:31 +0530

    foreground service

--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@ add redux toolkit
 
 * npm install @reduxjs/toolkit
 * npm install react-redux
+
+* npm i @supersami/rn-foreground-service
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,11 +2,13 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React ,{useEffect} from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
 import { signOut } from 'firebase/auth'
-import { auth } from '../config/firebase'
+import { auth, db } from '../config/firebase'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useDispatch } from 'react-redux'
 import { setUser } from '../redux/slices/user'
 import PushNotification from 'react-native-push-notification';
+import ReactNativeForegroundService from '@supersami/rn-foreground-service'
+import { collection, getDocs } from 'firebase/firestore'
 export default function HomeScreen() {
 
 const createChanels =()=>{
@@ -15,11 +17,34 @@ const createChanels =()=>{
     channelName:'Test Channel'
   })
 }
-
+const getData =async()=>{
+  const querySnapshot = await getDocs(collection(db, "location"));
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
+}
 useEffect(()=>{
   createChanels()
 },[])
+useEffect(() => {
+  ReactNativeForegroundService.add_task(() => { getData()}, {
+    delay: 10000,
+    onLoop: true,
+    taskId: 'taskid',
+    onError: e => console.log('Error logging:', e),
+  });
+ 
+}, []);
 
+const starttask = () => {
+  ReactNativeForegroundService.start({
+    id: 1,
+    title: 'My Foreground Service',
+    text: 'This is my foreground service',
+    icon: 'ic_notification',
+  });
+};
 
   const dispatch = useDispatch();
 
@@ -42,7 +67,7 @@ const handleNotify=()=>{
 
   return (<ScreenWrapper>
     <View className='flex-row justify-between items-center p-4'>
-    <TouchableOpacity className='p-2 px-3 bg-white border border-gray-200 rounded-full' onPress={handleNotify}>
+    <TouchableOpacity className='p-2 px-3 bg-white border border-gray-200 rounded-full' onPress={starttask}>
         <Text>Notify</Text>
       </TouchableOpacity>
       <View>
